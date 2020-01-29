c27f99559b2a02927714d365a1d57c334a3b1a0d
Bastien BG. GUILLOT <b.guillot@ALLIANCETELECOM>
2020-01-29 12:03:51 +0100

    catAT v1.1

--- catAT.sh
+++ catAT.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-aide="""
-Name :
-    catAT : Script permettant de dans un ou plusieurs fichiers ce que l'on souhaite
-Description :
-    Le programme est un cat avancé permettant de lire un ou plusieurs fichier (Pour lire plusieurs fichier, utiliser * -> par exemple : file* va lire file, file1, file68... )
-    Le programme peut aussi chercher un mot particulier au sein du fichier.
-Exemple:
-    catAT -h
-        Affiche l'aide
-
-    catAT -n fichier
-        va cat le(s) fichier(s) en question
-
-        On peut rajouter l'option -r test
-            Cette option permet de chercher un mot dans le(s) fichier(s)
-
-"""
-
-
 help="0"
 name="0"
 recherch="0"
+dossier="./"
 
-while getopts "n:r:" option
+while getopts "d:n:r:" option
 do
 case $option in
     n)
@@ -36,6 +18,9 @@ case $option in
         grep=$OPTARG
     ;;
 
+    d)
+        dossier=$OPTARG
+
     h)
         help="1"
     ;;
@@ -44,15 +29,51 @@ esac
 done
 
 if [ $help = "1" ];then
-    echo $aide
+    echo -e """\e[32m
+Name :
+    catAT : Script permettant de dans un ou plusieurs fichiers ce que l'on souhaite
+Description :
+    Le programme est un cat avancé permettant de lire un ou plusieurs fichier (Pour lire plusieurs fichier, utiliser * -> par exemple : file* va lire file, file1, file68... )
+    Le programme peut aussi chercher un mot particulier au sein du fichier.
+Exemple:
+    catAT -h
+        Affiche l'aide
+
+    catAT -n fichier
+        va cat le(s) fichier(s) en question
+
+        catAT -n fichier -r test
+            Cette option permet de chercher un mot dans le(s) fichier(s)
+        catAT -n fichier -d dossier
+            Cette option permet de choisir le dossier dans lequel le programme travaille
+\e[37m
+"""
 
 elif [ $name = "1" ];then
     if [ $recherch = "1" ];then
-        cat $fich | grep $grep
+        cat $dossier$fich | grep $grep
     else
-        cat $fich
+        cat $dossier$fich
     fi
     
 else
-    echo $aide
+    echo -e """\e[32m
+Name :
+    catAT : Script permettant de dans un ou plusieurs fichiers ce que l'on souhaite
+Description :
+    Le programme est un cat avancé permettant de lire un ou plusieurs fichier (Pour lire plusieurs fichier, utiliser * -> par exemple : file* va lire file, file1, file68... )
+    Le programme peut aussi chercher un mot particulier au sein du fichier.
+Exemple:
+    catAT -h
+        Affiche l'aide
+
+    catAT -n fichier
+        va cat le(s) fichier(s) en question
+
+        catAT -n fichier -r test
+            Cette option permet de chercher un mot dans le(s) fichier(s)
+        catAT -n fichier -d dossier
+            Cette option permet de choisir le dossier dans lequel le programme travaille
+\e[37m
+"""
 fi
